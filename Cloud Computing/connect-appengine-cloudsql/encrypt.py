f10ac8a2505b2befbbbcb51ea85a29bbb7d1d827
Vanz-Noob <renaldivanza@gmail.com>
2022-10-29 22:31:04 +0700

    feat: change DB, change API

--- Cloud Computing/connect-appengine-cloudsql/encrypt.py
+++
@@ -1,9 +0,0 @@
-from passlib.hash import sha256_crypt
-
-password = sha256_crypt.encrypt("re")
-password2 = sha256_crypt.encrypt("tono")
-
-print(password)
-print(password2)
-
-print(sha256_crypt.verify("re", password))
