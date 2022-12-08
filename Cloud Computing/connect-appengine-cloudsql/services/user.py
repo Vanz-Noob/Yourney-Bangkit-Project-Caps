b6a12995d00e7feb5af5c2521830678da20a8fba
mochamad rafli <rafliforbusiness@gmail.com>
2022-12-08 23:48:24 +0700

    feat: create user service

---
+++ Cloud Computing/connect-appengine-cloudsql/services/user.py
@@ -0,0 +1,26 @@
+import pymysql
+import os
+
+
+class UserService:
+    def __init__(self,db_user,db_password,db_name,db_connection_name):
+        self.db_user = db_user
+        self.db_password = db_password
+        self.db_name = db_name
+        self.db_connection_name = db_connection_name
+    
+    def check_existing_user(self, username, email):
+        if os.environ.get('GAE_ENV') == 'standard':
+            unix_socket = '/cloudsql/{}'.format(self.db_connection_name)
+            cnx = pymysql.connect(user=self.db_user, password=self.db_password,
+                                unix_socket=unix_socket, db=self.db_name)
+        else:
+            host = '127.0.0.1'
+            cnx = pymysql.connect(user=self.db_user, password=self.db_password,
+                                host=host, db=self.db_name)
+        with cnx.cursor() as cursor:
+            cursor.execute('SELECT id_user FROM user WHERE LOWER(username) = LOWER(%s) OR LOWER(email) = LOWER(%s);',(username, email))
+            result = cursor.fetchone()
+        cnx.close()
+
+        return result
