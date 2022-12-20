89bed6d9fe3f4442cc22d7a63e52fb069bfe2948
mochamad rafli <rafliforbusiness@gmail.com>
2022-12-20 12:15:45 +0700

    feat/add parameter for tweepy

--- Cloud Computing/connect-appengine-cloudsql/services/twitter.py
+++ Cloud Computing/connect-appengine-cloudsql/services/twitter.py
@@ -23,7 +23,7 @@ access_token_secret = config['twitter']['access_token_secret']
 
 auth = tweepy.OAuthHandler(api_key, api_key_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
 df_id = pd.read_csv("stopwords_id.txt", header=None)
 stopwords_id = df_id[0].to_list()
