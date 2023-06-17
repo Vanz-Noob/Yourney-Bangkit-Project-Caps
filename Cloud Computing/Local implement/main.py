da91519a5ad7716cf140950aca6a65b861dbab2a
Vanz-Noob <renaldivanza@gmail.com>
2023-06-17 19:52:10 +0700

    feat: update main.py route

--- Cloud Computing/Local implement/main.py
+++ Cloud Computing/Local implement/main.py
@@ -346,12 +346,12 @@ def logout():
 # endpoint to verify jwt token works properly
 # Protect a route with jwt_required, which will kick out requests
 # without a valid JWT present.
-# @app.route("/protected", methods=["GET"])
-# @jwt_required(refresh=False)
-# def protected():
-#     # Access the identity of the current user with get_jwt_identity
-#     current_user = get_jwt_identity()
-#     return jsonify(logged_in_as=current_user['username']), 200
+@app.route("/protected", methods=["GET"])
+@jwt_required(refresh=False)
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user['username']), 200
 
 # get user profile
 
